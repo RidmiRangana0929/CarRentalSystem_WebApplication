84adcc61ac14e3157a73ddba057b25c14d060783
RidmiRangana0929 <ridmirangana1429@gmail.com>
2023-02-26 14:30:48 +0530

    updated

--- BackEnd/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ BackEnd/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -9,11 +9,13 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
+@EnableTransactionManagement
 public class JPAConfig {
 
     @Bean
