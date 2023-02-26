a0d364a00a18e157fa324a910a31ad4924ab8dcc
RidmiRangana0929 <ridmirangana1429@gmail.com>
2023-02-27 00:06:15 +0530

    updated

--- BackEnd/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ BackEnd/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -32,6 +32,7 @@ public class JPAConfig {
     @Bean
     public DataSource dataSource(){
         DriverManagerDataSource ds=new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
         ds.setUrl("jdbc:mysql://localhost:3306/car_rental?createDatabaseIfNotExist=true");
         ds.setUsername("root");
         ds.setPassword("1234");
