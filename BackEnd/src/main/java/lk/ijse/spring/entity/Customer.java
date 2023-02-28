b0402a1b598eee3ddbda9d524c106106ad019ae6
RidmiRangana0929 <ridmirangana1429@gmail.com>
2023-02-28 13:38:42 +0530

    updated html file

--- BackEnd/src/main/java/lk/ijse/spring/entity/Customer.java
+++ BackEnd/src/main/java/lk/ijse/spring/entity/Customer.java
@@ -1,4 +1,29 @@
 package lk.ijse.spring.entity;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class Customer {
+    @Id
+    private String c_id;
+    private String c_name;
+    private String c_email;
+    private String c_nic;
+    private String c_license_no;
+    private String c_contact_no;
+    private String c_username;
+    private String c_password;
+    private String c_nic_img;
+    private String c_license_img;
 }
