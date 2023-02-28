6874939e9b5dd74ebe0fa784b7117fea67e5198b
RidmiRangana0929 <ridmirangana1429@gmail.com>
2023-02-28 23:33:13 +0530

    updated

--- BackEnd/src/main/java/lk/ijse/spring/entity/Rent.java
+++ BackEnd/src/main/java/lk/ijse/spring/entity/Rent.java
@@ -14,12 +14,12 @@ import javax.persistence.Id;
 @Data
 @ToString
 @Entity
-public class Driver {
+public class Rent {
     @Id
-    private String d_id;
-    private String d_name;
-    private String d_nic;
-    private String d_license_no;
-    private String d_contact_no;
-    private String d_address;
+    private String r_id;
+    private String r_date;
+    private String r_pickup;
+    private String r_return;
+    private String r_service;
+    private String r_bank_slip_img;
 }
