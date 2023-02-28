75ff1763e26ba1c492d89120170daec60e5cc7cc
RidmiRangana0929 <ridmirangana1429@gmail.com>
2023-02-28 14:21:35 +0530

    updated

--- BackEnd/src/main/java/lk/ijse/spring/dto/CustomerDTO.java
+++ BackEnd/src/main/java/lk/ijse/spring/dto/CustomerDTO.java
@@ -1,4 +1,20 @@
 package lk.ijse.spring.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class CustomerDTO {
+    private String d_id;
+    private String d_name;
+    private String d_nic;
+    private String d_license_no;
+    private String d_contact_no;
+    private String d_address;
 }
--- BackEnd/src/main/java/lk/ijse/spring/service/CustomerService.java
+++ BackEnd/src/main/java/lk/ijse/spring/service/CustomerService.java
@@ -1,4 +1,11 @@
 package lk.ijse.spring.service;
 
+import lk.ijse.spring.dto.CustomerDTO;
+
+import java.util.ArrayList;
+
 public interface CustomerService {
+    public void SignUpCustomer(CustomerDTO dto);
+
+    public ArrayList<CustomerDTO> getAllCustomers();
 }
