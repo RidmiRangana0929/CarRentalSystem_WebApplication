b449df0cec3478224c39c258d958ef62845d25cf
RidmiRangana0929 <ridmirangana1429@gmail.com>
2023-03-01 00:35:25 +0530

    updated

--- BackEnd/src/main/java/lk/ijse/spring/service/CarService.java
+++ BackEnd/src/main/java/lk/ijse/spring/service/CarService.java
@@ -1,4 +1,15 @@
 package lk.ijse.spring.service;
 
+import lk.ijse.spring.dto.CarDTO;
+
+import java.util.ArrayList;
+
 public interface CarService {
+    public void addCar(CarDTO dto);
+
+    public void deleteCar(String id);
+
+    public void updateCar(CarDTO dto);
+
+    public ArrayList<CarDTO> getAllCars();
 }
--- BackEnd/src/main/java/lk/ijse/spring/service/RentService.java
+++ BackEnd/src/main/java/lk/ijse/spring/service/RentService.java
@@ -1,4 +1,16 @@
 package lk.ijse.spring.service;
 
+import lk.ijse.spring.dto.CarDTO;
+import lk.ijse.spring.dto.RentDTO;
+
+import java.util.ArrayList;
+
 public interface RentService {
+    public void addRent(RentDTO dto);
+
+    public void deleteRent(String id);
+
+    public void updateRent(RentDTO dto);
+
+    public ArrayList<RentDTO> getAllRents();
 }
