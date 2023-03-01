1218c98f4d049239b8c6d2313d424f68fb5058b4
RidmiRangana0929 <ridmirangana1429@gmail.com>
2023-03-01 18:44:44 +0530

    updated

--- BackEnd/src/main/java/lk/ijse/spring/controller/CarController.java
+++ BackEnd/src/main/java/lk/ijse/spring/controller/CarController.java
@@ -1,4 +1,45 @@
 package lk.ijse.spring.controller;
 
+
+import lk.ijse.spring.dto.CarDTO;
+import lk.ijse.spring.dto.DriverDTO;
+import lk.ijse.spring.service.CarService;
+import lk.ijse.spring.service.DriverService;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/car")
+@CrossOrigin
 public class CarController {
+    @Autowired
+    private CarService service;
+
+
+    @PostMapping
+    public ResponseUtil saveCar(@ModelAttribute CarDTO dto) {
+        service.addCar(dto);
+        return new ResponseUtil("200",dto.getRegister_no()+"Added.!",null);
+    }
+
+    @PutMapping
+    public ResponseUtil updateCar(@RequestBody CarDTO dto){
+        service.updateCar(dto);
+        return new ResponseUtil("200",dto.getRegister_no()+": Updated.!",null);
+    }
+
+    @DeleteMapping(params = "id")
+    public ResponseUtil deleteCar(String id){
+        service.deleteCar(id);
+        return new ResponseUtil("200",id+": Deleted..!",null);
+    }
+
+    @GetMapping
+    public ResponseUtil getAllCars(){
+        ArrayList<CarDTO> allCars= service.getAllCars();
+        return new ResponseUtil("200","Success..!",allCars);
+    }
 }
--- BackEnd/src/main/java/lk/ijse/spring/service/impl/CarServiceImpl.java
+++ BackEnd/src/main/java/lk/ijse/spring/service/impl/CarServiceImpl.java
@@ -1,28 +1,66 @@
 package lk.ijse.spring.service.impl;
 
 import lk.ijse.spring.dto.CarDTO;
+import lk.ijse.spring.dto.DriverDTO;
+import lk.ijse.spring.entity.Car;
+import lk.ijse.spring.entity.Driver;
+import lk.ijse.spring.repo.CarRepo;
+import lk.ijse.spring.repo.DriverRepo;
 import lk.ijse.spring.service.CarService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 
+
+@Service
+@Transactional
 public class CarServiceImpl implements CarService {
+    @Autowired
+    private CarRepo repo;
+
+    @Autowired
+    private ModelMapper mapper;
+
+
     @Override
     public void addCar(CarDTO dto) {
+
+        if (repo.existsById(dto.getRegister_no())) {
+            throw new RuntimeException("Car"+dto.getRegister_no()+" Already Exist..!");
+        }
+        repo.save(mapper.map(dto, Car.class));
 
     }
 
     @Override
-    public void deleteCar(String id) {
+    public void deleteCar(String reg_no) {
+
+        if (!repo.existsById(reg_no)){
+            throw new RuntimeException("Car "+reg_no+" Not Available to Delete..!");
+        }
+        repo.deleteById(reg_no);
 
     }
 
     @Override
     public void updateCar(CarDTO dto) {
 
+        if (!repo.existsById(dto.getRegister_no())){
+            throw new RuntimeException("Car "+dto.getRegister_no()+" Not Available to Update..!");
+        }
+        repo.save( mapper.map(dto, Car.class));
+
     }
 
     @Override
     public ArrayList<CarDTO> getAllCars() {
-        return null;
+
+        return mapper.map(repo.findAll(), new TypeToken<ArrayList<CarDTO>>() {
+        }.getType());
+
     }
 }
